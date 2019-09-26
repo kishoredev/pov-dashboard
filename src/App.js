00c4fc16ee0bcf677c98324dbd1968c183467d91
Kishore <kishoreniyogi@gmail.com>
2019-09-26 16:57:22 +0800

    Images added

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
+import D1Image from "./D1.JPG";
+import D2Image from "./D2.JPG";
+import D3Image from "./D3.JPG";
+import D4Image from "./D4.JPG";
+ 
 function App() {
   return ( 
     <div className="App">
       <header className="App-header">
 		<div>DevSecOps Executive Dashboard </div>
       </header>
-	<div className="widget">Dashboard 1</div>
-	<div className="widget">Dashboard 2</div>
-	<div className="widget">Dashboard 3</div>
-	<div className="widget">Dashboard 4</div>
-
+	  <table width="100%">
+		<tr>
+			<td width="70%"><img width="100%" height="100%" src={D1Image}/></td>
+			<td width="30%"><img width="100%" height="100%"  src={D3Image}/></td>
+		</tr>
+		<tr>
+			<td width="70%"><img width="100%" height="100%" src={D2Image}/></td>
+			<td width="30%"><img width="100%" height="100%"  src={D4Image}/></td>
+		</tr>
+	  </table>
     </div>
  
   );
