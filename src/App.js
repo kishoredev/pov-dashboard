3e33f4c5d2fb27176c9a3599a4831bf51693f991
Kishore <kishoreniyogi@gmail.com>
2019-09-26 14:13:07 +0800

    Changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return ( 
     <div className="App">
       <header className="App-header">
 		<div>DevSecOps Executive Dashboard </div>
       </header>
-	<div className="widget"><iframe className="embed-container" src="http://pov-dashboard.s3-website-us-east-1.amazonaws.com/" /></div>
-    <div className="widget"><iframe className="embed-container" src="http://www.eenadu.net" /></div>
-    <div className="widget"><iframe className="embed-container" src="http://pov-dashboard.s3-website-us-east-1.amazonaws.com/" /></div>
-    <div className="widget"><iframe className="embed-container" src="http://pov-dashboard.s3-website-us-east-1.amazonaws.com/" /></div>
-    </div>
+	<div className="widget">Dashboard 1</div>
+	<div className="widget">Dashboard 2</div>
+	<div className="widget">Dashboard 3</div>
+	<div className="widget">Dashboard 4</div>
 
+    </div>
+ 
   );
 }
 
